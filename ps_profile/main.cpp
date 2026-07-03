17efa938288601315c415e25ac8fe6163e7925a8
Mohamed Hassan <muhammad.hamdy.hassan@gmail.com>
2026-07-03 17:09:33 +0300

    Validate Variant B PS-controlled PL SGM with timing breakdown

--- ps_profile/main.cpp
+++ ps_profile/main.cpp
@@ -27,6 +27,9 @@ int main()
 			disp_hw[r][c] = 0;
 		}
 	}
+	XTime_GetTime(&t1);
+
+	XTime t_input = t1 - t0;
 
 	XSgm_kernel pl_accel;
 	int status = XSgm_kernel_Initialize(&pl_accel,XPAR_SGM_KERNEL_0_DEVICE_ID);
@@ -40,17 +43,28 @@ int main()
 	XSgm_kernel_Set_right_r(&pl_accel, (UINTPTR)&right_buf[0][0]);
 	XSgm_kernel_Set_disp(&pl_accel, (UINTPTR)&disp_hw[0][0]);
 
+	XTime_GetTime(&t0);
 	Xil_DCacheFlushRange((UINTPTR)&left_buf[0][0], IMG_H * IMG_W * sizeof(uint8_t));
 	Xil_DCacheFlushRange((UINTPTR)&right_buf[0][0], IMG_H * IMG_W * sizeof(uint8_t));
 	Xil_DCacheFlushRange((UINTPTR)&disp_hw[0][0], IMG_H * IMG_W * sizeof(uint8_t));
-
-	XSgm_kernel_Start(&pl_accel);
-	while(!XSgm_kernel_IsDone(&pl_accel));
-
-	Xil_DCacheInvalidateRange((UINTPTR)&disp_hw[0][0], IMG_H * IMG_W * sizeof(uint8_t));
-
 	XTime_GetTime(&t1);
 
+	XTime t_cache_flush = t1 - t0;
+
+	XTime_GetTime(&t0);
+	XSgm_kernel_Start(&pl_accel);
+	while(!XSgm_kernel_IsDone(&pl_accel));
+	XTime_GetTime(&t1);
+
+	XTime t_pl= t1 - t0;
+
+	XTime_GetTime(&t0);
+	Xil_DCacheInvalidateRange((UINTPTR)&disp_hw[0][0], IMG_H * IMG_W * sizeof(uint8_t));
+	XTime_GetTime(&t1);
+
+	XTime t_cache_invalidate = t1 - t0;
+
+	XTime_GetTime(&t0);
 	volatile uint32_t checksum = 0;
 	for(int r = 0; r < IMG_H; ++r)
 	{
@@ -59,15 +73,22 @@ int main()
 			checksum += disp_hw[r][c];
 		}
 	}
+	XTime_GetTime(&t1);
+	XTime t_checksum = t1 - t0;
 
-	uint64_t cycles = uint64_t(t1 - t0);
-    uint32_t us = (uint32_t)((cycles * 1000000ULL) / COUNTS_PER_SECOND);
+	XTime t_variant_b = t_input + t_cache_flush + t_pl + t_cache_invalidate;
 
 	xil_printf("SGM finished. \r\n");
 	xil_printf("checksum = %u\r\n", (unsigned int)checksum);
-	xil_printf("Timer counts = %u\r\n", (unsigned int)cycles);
-    xil_printf("Variant B latency = %u us\r\n", (unsigned int)us);
-    xil_printf("Variant B latency = %u ms\r\n", (unsigned int)(us / 1000));
+
+    xil_printf("data input time = %u us \r\n", (unsigned)((t_input * 1000000ULL) / COUNTS_PER_SECOND));
+    xil_printf("cache flush time = %u us \r\n", (unsigned)((t_cache_flush * 1000000ULL) / COUNTS_PER_SECOND));
+    xil_printf("PL accelerator time = %u us \r\n", (unsigned)((t_pl * 1000000ULL) / COUNTS_PER_SECOND));
+    xil_printf("cache invalidate time = %u us \r\n", (unsigned)((t_cache_invalidate * 1000000ULL) / COUNTS_PER_SECOND));
+    xil_printf("checksum time = %u us \r\n", (unsigned)((t_checksum * 1000000ULL) / COUNTS_PER_SECOND));
+
+    xil_printf("Variant B latency = %u us\r\n", (unsigned int)((t_variant_b * 1000000ULL) / COUNTS_PER_SECOND));
+    xil_printf("Variant B latency = %u ms\r\n", (unsigned int)((t_variant_b * 1000ULL) / COUNTS_PER_SECOND));
 
 	xil_printf("Counts per second = %d\r\n", COUNTS_PER_SECOND);
 
